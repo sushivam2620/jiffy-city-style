fd0a48bdfa9261334ecb58e41cddaa4998034784
sushivam2620 <sushivam2620@gmail.com>
2025-12-13 22:25:32 +0530

    Update vite.config.ts
    fix: allow render host

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      ".onrender.com",
+      "drapeit.in",
+      "www.drapeit.in",
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
