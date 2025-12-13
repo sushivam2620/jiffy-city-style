1ea422f14d95fd87f4f8c604184b7923561893b0
sushivam2620 <sushivam2620@gmail.com>
2025-12-13 22:34:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [
+    react({
+      jsxImportSource: "react",
+    }),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   server: {
     host: "::",
     port: 8080,
@@ -14,7 +20,11 @@ export default defineConfig(({ mode }) => ({
       "www.drapeit.in",
     ],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  build: {
+    target: "esnext",
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
